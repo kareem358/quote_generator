05bf3d61cf45c45da83a39894999f8fb1c0f52a1
kareem358 <kareemullahafridi10@gmail.com>
2025-07-18 22:58:59 +0500

    ight and dark theme code in the material app

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,40 @@ class QuoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Random Quote Generator',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black87),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.deepPurple,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
+      ),
+      themeMode: ThemeMode.system, // Use system preference
+      home: const HomeScreen(),
+    );
+
+    /*MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quote Generator',
       theme: ThemeData(
           textTheme: GoogleFonts.poppinsTextTheme(),
           primarySwatch: Colors.deepPurple,
         ),
       home: const HomeScreen(),
-    );
+    );*/
   }
 }
