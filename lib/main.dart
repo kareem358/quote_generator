8940262f93b7de8345000d1d33032dba26a94e3a
kareem358 <kareemullahafridi10@gmail.com>
2025-07-18 21:14:54 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen.dart';
 
 void main() {
@@ -13,7 +14,10 @@ class QuoteApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Quote Generator',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          primarySwatch: Colors.deepPurple,
+        ),
       home: const HomeScreen(),
     );
   }
