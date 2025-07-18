dd5e57d7df496029b5c85c441fbb5b5bfb7f6614
kareem358 <kareemullahafridi10@gmail.com>
2025-07-18 13:01:55 +0500

    quote model and quote data list

---
+++ lib/utils/quote_data.dart
@@ -0,0 +1,8 @@
+import '../models/quote_model.dart';
+
+List<Quote> quotes = [
+  Quote(text: "Believe in yourself.", author: "Anonymous"),
+  Quote(text: "Stay hungry, stay foolish.", author: "Steve Jobs"),
+  Quote(text: "Simplicity is the ultimate sophistication.", author: "Leonardo da Vinci"),
+  Quote(text: "You miss 100% of the shots you don’t take.", author: "Wayne Gretzky"),
+];
